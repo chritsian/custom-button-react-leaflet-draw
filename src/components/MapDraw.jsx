40433864eb9c94f4d66e140b61e0adb63ae8abaa
Christian Zielke <christian.zielke@exxeta.com>
2020-09-20 15:24:34 +0200

    added assets folder with leaflet-draw css, completed example

--- src/components/MapDraw.jsx
+++ src/components/MapDraw.jsx
@@ -2,8 +2,9 @@ import React, { useRef, useState } from "react";
 
 import { Map, FeatureGroup, TileLayer } from "react-leaflet";
 import { EditControl } from "react-leaflet-draw";
+import L from "leaflet"
 
-import "leaflet-draw/dist/leaflet.draw-src.css"
+import "./assets/leaflet.draw.css"
 
 // Material components
 import { makeStyles, Button } from "@material-ui/core";
@@ -22,35 +23,95 @@ const useStyles = makeStyles(theme => ({
         marginBottom: "8%",
         transform: "translateX(-50%)",
     },
+    headerWrapper: {
+        zIndex: 1,
+        marginLeft: theme.spacing(3),
+        marginTop: theme.spacing(1),
+    }
 }));
 
 export const MapDraw = (props) => {
 
     const classes = useStyles(props)
-    const editControl = useRef();
+    const editRef = useRef();
 
     //use states to enable the button clicks at special times, also you can use states 
     //to perform multiple actions with one button
 
     const [drawing, setDrawing] = useState(false);
-    
+
+    //uncomment next line to change a default tooltip text
+    //L.drawLocal.draw.handlers.polygon.tooltip.start = "This is an modified tooltip text"
+
 
     const handleClick = () => {
         
         //Edit this method to perform other actions
 
         if (!drawing) {
-            editControl.current.leafletElement._toolbars.draw._modes.polygon.handler.enable()
+            editRef.current.leafletElement._toolbars.draw._modes.polygon.handler.enable()
         } else {
-            editControl.current.leafletElement._toolbars.draw._modes.polygon.handler.completeShape()
-            editControl.current.leafletElement._toolbars.draw._modes.polygon.handler.disable()
+            editRef.current.leafletElement._toolbars.draw._modes.polygon.handler.completeShape()
+            editRef.current.leafletElement._toolbars.draw._modes.polygon.handler.disable()
         }
         setDrawing(!drawing)
     }
 
+    //Overview of methods you can use to access the toolbars handler for polygon drawing
+
+        // startDraw
+        //editRef.current.leafletElement._toolbars.draw._modes.polygon.handler.enable()
+
+        // cancelDraw 
+        //editRef.current.leafletElement._toolbars.draw._modes.polygon.handler.disable()
+
+        // vertexBack 
+        //editRef.current.leafletElement._toolbars.draw._modes.polygon.handler.deleteLastVertex()
+
+        // stopDraw 
+        //editRef.current.leafletElement._toolbars.draw._modes.polygon.handler.completeShape()
+        //editRef.current.leafletElement._toolbars.draw._modes.polygon.handler.disable()
+
+        // startEdit 
+        //editRef.current.leafletElement._toolbars.edit._modes.edit.handler.enable()
+
+        // cancelEdit 
+        //editRef.current.leafletElement._toolbars.edit._modes.edit.handler.disable()
+
+        // saveEdit
+        //editRef.current.leafletElement._toolbars.edit._modes.edit.handler.save() 
+        //editRef.current.leafletElement._toolbars.edit._modes.edit.handler.disable()
+        
+        // startDelete 
+        //editRef.current.leafletElement._toolbars.edit._modes.remove.handler.enable()
+
+        // saveDelete 
+        //editRef.current.leafletElement._toolbars.edit._modes.remove.handler.save()
+        //editRef.current.leafletElement._toolbars.edit._modes.remove.handler.disable()
+
+    
+    //example methods from section 4
+
+    const onShapeDrawn = (e) => {
+        setDrawing(false)
+        e.layer.on('click', () => {
+            editRef.current.leafletElement._toolbars.edit._modes.edit.handler.enable()
+        })
+        e.layer.bindTooltip(
+            "This is an example tooltip", 
+            {
+              className: 'leaflet-draw-tooltip leaflet-draw-tooltip-visible',
+              sticky: true,
+              direction: 'right'
+            }
+        );
+    }
+
     return (
         <div>
-            <h1> Custom button react-leaflet-draw Demo </h1>
+            <div className={classes.headerWrapper}>
+                <h1> Custom button react-leaflet-draw Demo </h1>
+            </div>
             
             <Map 
             center={[51.515, -0.09]} 
@@ -65,9 +126,9 @@ export const MapDraw = (props) => {
 
                 <FeatureGroup >
                     <EditControl
-                    ref={editControl}
+                    ref={editRef}
                     position='topright'
-
+                    onCreated={onShapeDrawn}
                     //here you can specify your shape options and which handler you want to enable
                     draw={{
                         rectangle: false,
